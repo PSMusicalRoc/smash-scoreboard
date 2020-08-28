dd59a1434023015499c38256a8910d116579db42
Tim Bishop <timbishop514@gmail.com>
2020-08-28 08:19:25 -0400

    Added SIZE_MODE to globals

--- Widgets/globals.py
+++ Widgets/globals.py
@@ -8,3 +8,13 @@
 #
 _important_vars = {}
 dialog = None
+
+# Here lies code for a new feature, size changes (in this
+# case, resolution modes). The idea is that the user can choose
+# how large the program is from a list and save that to an options
+# file.
+#
+# Potential values for this variable can be:
+# - "1920x1080"
+# - "1280x720"
+SIZE_MODE = "1280x720"
